0509ae793291ca2f8ab0cb5b78631b835981915f
lipelobato <31655914+lipelobato@users.noreply.github.com>
2017-12-07 09:41:33 -0200

    fim de aula

--- aula20170905/seq3.c
+++ aula20170905/seq3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <conio.h>
 int main()
 {
     float a, b, c, d, e, peso2, peso3, peso5, media;
@@ -28,6 +28,6 @@ int main()
     media = (a*peso2 + b*peso2 + c*peso2 + d*peso3 + e*peso5)/(peso2+peso3+peso5);
     printf("A media ponderada eh: %.2f \n\n", media);
 
-    system("pause");
+    getche();
     return 0;
 }
